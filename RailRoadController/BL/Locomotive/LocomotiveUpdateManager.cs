acc3afd71bb457c19204a646ef3724b0ecd32d53
rrobuschi <rrobuschi@gmail.com>
2019-02-14 22:25:25 +0100

    Added LocomotiveUpdateManager with tests

---
+++ RailRoadController/BL/Locomotive/LocomotiveUpdateManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Timers;
+using RailRoadController.BL.DccCommand;
+
+namespace RailRoadController.BL.Locomotive
+{
+    public class LocomotiveUpdateManager
+    {
+        private readonly ConcurrentQueue<string> _commandQueue;
+        private readonly IDccCommandBuilder _dccCommandBuilder;
+        private readonly IDccCommandSender _dccCommandSender;
+        private readonly Timer _timer;
+
+        public LocomotiveUpdateManager(List<Locomotive> fleet, IDccCommandBuilder dccCommandBuilder, IDccCommandSender dccCommandSender)
+        {
+            _dccCommandBuilder = dccCommandBuilder;
+            _dccCommandSender = dccCommandSender;
+            _commandQueue = new ConcurrentQueue<string>();
+            _timer = new Timer(100);
+            _timer.Elapsed += TimerElapsed;
+
+            foreach (var locomotive in fleet)
+            {
+                locomotive.FunctionChanged += LocomotiveFunctionChanged;
+                locomotive.MovementChanged += LocomotiveMovementChanged;
+            }
+
+            _timer.Start();
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            _timer.Stop();
+            while (_commandQueue.TryDequeue(out var dccCommand))
+            {
+                _dccCommandSender.SendCommand(dccCommand);
+            }
+            _timer.Start();
+        }
+
+        private void LocomotiveMovementChanged(object sender, EventArgs e)
+        {
+            var locomotive = (Locomotive)sender;
+
+            var dccCommand = _dccCommandBuilder.BuildCommand(locomotive.Address, locomotive.ProjectedPower.ToString(), locomotive.Direction.ToString());
+
+            _commandQueue.Enqueue(dccCommand);
+        }
+
+        private void LocomotiveFunctionChanged(object sender, EventArgs e)
+        {
+            var locomotive = (Locomotive)sender;
+
+            var dccCommand = _dccCommandBuilder.BuildCommand(locomotive.Address, locomotive.Functions);
+
+            _commandQueue.Enqueue(dccCommand);
+        }
+    }
+}
---
+++ RailRoadControllerTest/BL/Locomotive/LocomotiveUpdateManagerTest.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using FluentAssertions;
+using NSubstitute;
+using RailRoadController.BL.DccCommand;
+using RailRoadController.BL.Locomotive;
+using RailRoadController.Entities;
+
+namespace RailRoadControllerTest.BL.Locomotive
+{
+    public class LocomotiveUpdateManagerTest
+    {
+        private LocomotiveUpdateManager _sut;
+        private List<RailRoadController.BL.Locomotive.Locomotive> _fleet;
+        private IDccCommandBuilder _dccCommandBuilder;
+        private IDccCommandSender _dccCommandSender;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fleet = new List<RailRoadController.BL.Locomotive.Locomotive>
+            {
+                new RailRoadController.BL.Locomotive.Locomotive
+                {
+                    Address = "01", Name = "locomotive 1", Direction = 0, ProjectedPower = 0
+                }
+            };
+            _dccCommandBuilder = Substitute.For<IDccCommandBuilder>();
+            _dccCommandSender = Substitute.For<IDccCommandSender>();
+            _sut = new LocomotiveUpdateManager(_fleet, _dccCommandBuilder, _dccCommandSender);
+        }
+
+        [Test]
+        public void LocomotiveUpdateManager_object_exists()
+        {
+            _sut.Should().NotBeNull();
+        }
+
+        [Test]
+        public void an_update_to_locomotive_power_causes_a_dcc_command_to_be_sent()
+        {
+            _dccCommandBuilder.BuildCommand("01", "50", "0").Returns("dccCommand50");
+
+            var locomotive = _fleet[0];
+            locomotive.SetPower(50);
+
+            Thread.Sleep(150);
+
+            _dccCommandSender.Received().SendCommand("dccCommand50");
+        }
+
+        [Test]
+        public void an_update_to_locomotive_direction_causes_a_dcc_command_to_be_sent()
+        {
+            _dccCommandBuilder.BuildCommand("01", "0", "1").Returns("dccCommanddir1");
+
+            var locomotive = _fleet[0];
+            locomotive.ToggleDirection();
+
+            Thread.Sleep(150);
+
+            _dccCommandSender.Received().SendCommand("dccCommanddir1");
+        }
+
+        [Test]
+        public void an_update_to_locomotive_function_causes_a_dcc_command_to_be_sent()
+        {
+            FunctionSet functions ;
+            _dccCommandBuilder.BuildCommand("01", Arg.Is<FunctionSet>(x => x.F1)).Returns("dccCommandfun1");
+
+            var locomotive = _fleet[0];
+            locomotive.ToggleFunction(1);
+
+            Thread.Sleep(150);
+
+            _dccCommandSender.Received().SendCommand("dccCommandfun1");
+        }
+
+    }
+}
